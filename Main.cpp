8d2c9994e33c5998c41720f9f35fa6987fd88702
BigJt <BigJt@PANDASLAPTOP>
2026-08-27 23:43:14 -0400

    Added the rectangle-validation.

--- Main.cpp
+++ Main.cpp
@@ -6,9 +6,16 @@ int main()
 {
 	// Create a valid rectangle object
 	Rectangle box(5.0, 3.0);
-	cout << "Valid Rectangle: " << endl;
-	cout << "Length: " << box.getLength() << endl;
-	cout << "Width: " << box.getWidth() << endl;
-	cout << "Area: " << box.getArea() << endl;
+	if (box.Valid())
+	{
+		cout << "Valid Rectangle: " << endl;
+		cout << "Length: " << box.getLength() << endl;
+		cout << "Width: " << box.getWidth() << endl;
+		cout << "Area: " << box.getArea() << endl;
+	}
+	else
+	{
+		cout << "The rectangle is invalid." << endl;
+	}
 	return 0;
 }
